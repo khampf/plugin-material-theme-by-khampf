827a5a48c4e642a18e44fa6f0ce85e47312efc9c
khampf <khampf@users.sourceforge.net>
2017-02-14 15:20:12 +0200

    Only overriding the theme head. Header and top will stay default.

--- Plugin.php
+++ Plugin.php
@@ -8,7 +8,7 @@ class Plugin extends Base
     public function initialize()
     {
         $this->template->hook->attach('template:layout:head', 'theme:layout/head');
-        $this->template->hook->attach('template:layout:top', 'theme:layout/top');
-        $this->template->setTemplateOverride('header', 'theme:layout/header');
+//        $this->template->hook->attach('template:layout:top', 'theme:layout/top');
+//        $this->template->setTemplateOverride('header', 'theme:layout/header');
     }
 }
